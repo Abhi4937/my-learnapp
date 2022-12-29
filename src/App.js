2e14d3122447bf6d0b158e055fad8a8910bfd0e4
Abhi4937 <singhabhishek4937@gmail.com>
2022-12-29 13:55:07 +0530

    added tick function which calls the root.render every second using setInterval

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
-
+import ClockTick from './components/ClockTick';
 function App() {
   const element=<h1>Hello, World</h1>
   return (
     <div className="App">
           Learn React
           {element}
+          <ClockTick/>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -4,8 +4,11 @@ import './index.css';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+function tick(){
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
+}
+setInterval(tick,1000);

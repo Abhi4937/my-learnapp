00c18b0223e9785a83c882f89bc2fde47104c3cb
Abhi4937 <singhabhishek4937@gmail.com>
2023-01-02 18:30:06 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
 import User from './components/User';
 import Clock from './components/Clock';
+import Comment from './components/Comment';
+
 function App() {
   const element=<h1>Hello, World</h1>
   return (
@@ -9,6 +11,7 @@ function App() {
           {element}
           <User name='Abhishek'/>
           <Clock date={new Date().toLocaleTimeString()}/>
+          <Comment users={{name:"Abhishek"}} text="my comment text" date={new Date().toLocaleTimeString()}/>
     </div>
   );
 }

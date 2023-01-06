105349148fd57600ab96f9923f08e36b4bfed998
Abhi4937 <singhabhishek4937@gmail.com>
2023-01-06 23:11:12 +0530

    handling multiple inputs using computed property name

--- src/components/ControlledComponent.js
+++ src/components/ControlledComponent.js
@@ -4,29 +4,22 @@ class ControlledComponent extends Component {
     constructor(props){
         super(props);
         this.handleonsubmit=this.handleonsubmit.bind(this);
-        this.handleonchangetext=this.handleonchangetext.bind(this);
-        this.handleonchangetextArea=this.handleonchangetextArea.bind(this);
-        this.handleonchangeSelect=this.handleonchangeSelect.bind(this);
+        this.handleonchange=this.handleonchange.bind(this);
         this.state={
             textValue: "this is default text value type to change it",
             textAreaValue: "this is textarea value type a para",
             selectValue: "orange"
         }
     }
-    handleonchangetext(event){
-        this.setState((state)=>({textValue:event.target.value}));
-    }
-    handleonchangetextArea(event){
-        this.setState((state)=>({textAreaValue:event.target.value}));
-    }
+    handleonchange(event){
+        this.setState((state)=>({[event.target.name]:event.target.value}))
+    }    
+
     handleonsubmit(event){
         event.preventDefault();
         alert("your fav fruit is:"+this.state.selectValue);
     }
 
-    handleonchangeSelect(event){
-        this.setState((state)=>({selectValue : event.target.value}));
-    }
 
     render() {
         return (
@@ -35,15 +28,15 @@ class ControlledComponent extends Component {
                     <fieldset className='fs1'>
                     <legend>MY Form:</legend>
                     <label htmlFor="text-id">Text Box:</label>
-                    <input type="text" id="text-id" value={this.state.textValue} onChange={this.handleonchangetext}/>
+                    <input type="text" name="textValue" id="text-id" value={this.state.textValue} onChange={this.handleonchange}/>
                     <label htmlFor="textarea-id">Text Area:</label>
-                    <textarea id="textarea-id" value={this.state.textAreaValue} onchange={this.handleonchangetextArea}/>
+                    <textarea id="textarea-id" name="textAreaValue" value={this.state.textAreaValue} onChange={this.handleonchange}/>
                     <label>Select you fav fruit</label>
-                    <select value={this.state.selectValue} onChange={this.handleonchangeSelect}>
+                    <select name="selectValue" value={this.state.selectValue} onChange={this.handleonchange}>
                         <option value="grapefruit">grapefruit</option>
                         <option value="orange">orange</option>
-                        <option value="mango">orange</option>
-                        <option value="jackfruit">orange</option>
+                        <option value="mango">mango</option>
+                        <option value="jackfruit">jackfruit</option>
                     </select>
                     <button type="submit">Submit</button>
                     </fieldset>
